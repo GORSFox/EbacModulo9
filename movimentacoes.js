6013c5d014799e84b02a6f8542d04cccc77ffff7
Glaucia Rapozo <97680820+GORSFox@users.noreply.github.com>
2022-09-08 12:33:47 -0300

    Add outras movimentacoes

--- movimentacoes.js
+++ movimentacoes.js
@@ -24,10 +24,14 @@ class Registro {
 
     resumo(){
         let valorAtualizado = 0
+        let tipo = ''
         this.movimentacoes.forEach(lancamento => {
+            if(lancamento.nome.match(/imposto/)){ /* ! antes de lançamento - sld sem imposto - se retirar ! valor só do imposto */
             valorAtualizado += lancamento.saldo
+            tipo += lancamento.nome + '|'
+            }
         })
-        return valorAtualizado
+        return tipo + valorAtualizado
     }
 
     }
@@ -37,12 +41,11 @@ class Registro {
     const m02 = new Movimentacao('Bank X', 'comissao', 2000)
     const m03 = new Movimentacao('Bank X', 'imposto', -5000)
     const m04 = new Movimentacao('Bank X', 'contas', -1000)
-
+    const m05 = new Movimentacao('Bank X', 'imposto', -1000)
 
     const registro01 = new Registro(22,10,2021)
-    registro01.novaMovimentacao(m01, m02, m03, m04)
+    registro01.novaMovimentacao(m01, m02, m03, m04, m05)
     console.log('O saldo dos registros foi de:' +registro01.resumo())
 
 
 
-    
